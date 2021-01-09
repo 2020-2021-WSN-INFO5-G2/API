f79437c29a7aa7201f959fd21d8078530eab946b
Arxwel <romainpasdeloup@outlook.com>
2021-01-09 14:51:41 +0100

    Added fakedata to API

---
+++ README.md
@@ -0,0 +1,15 @@
+# ELora Holmes API
+We assume that you already have an application created and registered to TTN.
+
+1. Create a `.env` file with the following content and paste your application ID as well as your Access key
+```
+APPID=
+ACCESS_KEY=
+```
+
+2. Install the dependencies
+`npm install`
+
+3. Launch the API
+`npm start`
+
--- index.js
+++ index.js
@@ -1,23 +1,131 @@
-import { data } from "ttn";
-require('dotenv').config();
+import { data } from 'ttn';
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from "cors";
+import bodyParser from "body-parser";
 
-const appID = process.env.APPID
-const accessKey = process.env.ACCESS_KEY
+dotenv.config();
+
+var app = express();
+app.disable('x-powered-by')
+app.use(cors());
+app.use(bodyParser.json());
+const appID = process.env.APPID;
+const accessKey = process.env.ACCESS_KEY;
+const port = process.env.PORT || 3000;
+
+let frames = {
+  "devCartan": {
+    "app_id": "explorer-sherlock",
+    "dev_id": "devCartan",
+    "hardware_serial": "00FF3E80F4AE3F52",
+    "port": 4,
+    "counter": 2,
+    "payload_raw": {
+      "type": "Buffer",
+      "data": [19, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    "metadata": {
+      "time": "2020-12-07T19:29:46.717214946Z",
+      "frequency": 868.5,
+      "modulation": "LORA",
+      "data_rate": "SF9BW125",
+      "airtime": 246784000,
+      "coding_rate": "4/5",
+      "gateways": [
+        {
+          "gtw_id": "eui-3535303229007200",
+          "timestamp": 3067929442,
+          "time": "",
+          "channel": 2,
+          "rssi": -59,
+          "snr": 7.5,
+          "rf_chain": 0
+        },
+        {
+          "gtw_id": "eui-904d4afffeff4d8e",
+          "timestamp": 2824818076,
+          "time": "2020-12-07T19:29:45.666326Z",
+          "antenna": 1,
+          "channel": 3,
+          "rssi": -113,
+          "snr": -9.8,
+          "rf_chain": 0,
+          "latitude": 45.18541,
+          "longitude": 5.74257,
+          "altitude": 251
+        },
+        {
+          "gtw_id": "eui-904d4afffeff4dd3",
+          "timestamp": 1123636204,
+          "time": "2020-12-07T19:29:45.666323Z",
+          "channel": 7,
+          "rssi": -117,
+          "snr": -6,
+          "rf_chain": 0,
+          "latitude": 45.1709,
+          "longitude": 5.74149,
+          "altitude": 246
+        }]
+    },
+    "payload": {
+      "type": "Buffer",
+      "data": [19, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    "_msgid": "4e8cc0ac.df478"
+  }
+};
+
 
 const main = async function () {
   const client = await data(appID, accessKey)
+
+  app.get('/', function (req, res) {
+    res.send('hello world');
+
+  });
+
+  app.get('/devices/:device([a-z\-\_]+)', (req, res) => {
+    if (frames[req.params.device] === undefined) {
+      res.status(404)
+      res.send("Device not found")
+    } else {
+      res.status(200)
+      res.contentType('json')
+      res.send(JSON.stringify(frames[req.params.device]))
+    }
+  });
+
+  app.post('/devices/:device([a-z\-\_]+)', (req, res) => {
+    if (req.body.ledstatus === undefined) {
+      res.status(400)
+      res.send("You must specify what to do with the LED")
+    } else if (frames[req.params.device] === undefined) {
+      res.status(404)
+      res.send("Device not found")
+    } else {
+      client.send(req.params.device, Buffer.from([0x0f, req.body.ledstatus ? 0x01 : 0x00]))
+      res.status(200)
+      res.send("Instruction send to device")
+    }
+  });
 
   client
     .on("uplink", function (devID, payload) {
       console.log("Received uplink from ", devID)
       console.log(payload)
-      console.log(payload.metadata.gateways)
-      // send downlink
-      client.send(devID, Buffer.from([ 0x0f, 0xaf ]))
+      if (frames[devID] === undefined)
+        frames[devID] = []
+      frames[devID].push(payload)
+      if (frames[devID].length > 5)
+        frames[devID].shift()
     })
 }
 
 main().catch(function (err) {
   console.error(err)
-  process.exit(1)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
